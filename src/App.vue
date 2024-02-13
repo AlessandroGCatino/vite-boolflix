b312b70e706ddfe0c9c6451e5e55b73887df68e6
Alessandro Catino <148718244+AlessandroGCatino@users.noreply.github.com>
2024-02-13 15:54:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+    import axios from 'axios'
+    import { store } from './store'
+    
+    export default {
+        data() {
+            return{
+                store,
+            }
+        }
+    }
+
+
+</script>
+
+
+<style lang="scss">
+
+@use "./styles/general.scss";
+
+</style>
